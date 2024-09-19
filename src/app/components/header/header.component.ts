f7f772ae7cd80e1085ea58800da01ff3ad133dc2
vinitha26-ui <vinitharaghuraman@gmail.com>
2024-09-19 16:50:49 +0530

    portfolio chnges

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, NavigationStart, Router } from '@angular/router';
 import { ContentService } from 'src/modules/main-layout/services/content.service';
 
 @Component({
@@ -10,7 +11,7 @@ export class HeaderComponent implements OnInit {
   navigationItems: any[] = [];
   name: string;
 
-  constructor(private contentService: ContentService) {
+  constructor(private contentService: ContentService,private router: Router) {
     this.name = 'Vinitha R';
     this.navigationItems = [
       {
@@ -25,6 +26,20 @@ export class HeaderComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.router.events.subscribe((value:any) => {
+      if(value as NavigationStart){
+        if(value.url){
+        const url : any = value.url ;
+        if(!url.includes('hire')){
+          window.sessionStorage.removeItem('hirePage');
+          this.contentService.setHirePage(false);
+        }else {
+          window.sessionStorage.setItem('hirePage','true');
+          this.contentService.setHirePage(true);
+        }
+      }
+      }
+  });
   }
   onNavigationClick() {
     window.sessionStorage.removeItem('hirePage');
